6a8d82a9532349af4f664e09bcea45818aabf209
Muhammed Cihan Alkan <muhammedcihanalkan1@gmail.com>
2026-07-26 13:46:33 +0300

    kullanılmayan dosyalar temizlendi

--- src/alns/limits.py
+++
@@ -1,3 +0,0 @@
-MAX_SPOT = 500
-MAX_RELAY_CANDIDATES = 4       # hat basina en fazla 1-aktarmali (2 bacak) aday sayisi
-MAX_2HOP_CANDIDATES = 3        # hat basina en fazla 2-aktarmali (3 bacak) aday sayisi
